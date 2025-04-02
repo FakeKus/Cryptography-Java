97b839b793054aa8b7dac2fdcd0e1beb661f5a82
FakeKus <88687338+FakeKus@users.noreply.github.com>
2025-04-02 16:50:02 -0300

    initial

---
+++ src/main/java/org/alexandre/Main.java
@@ -0,0 +1,9 @@
+package org.alexandre;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Selecione uma opção: ");
+        System.out.println("\t1 - Consultar");
+        System.out.println("\t2 - Cadrastrar");
+    }
+}
